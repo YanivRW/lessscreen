92b18c071810e6a94f9f97712f7e5f32d053e82f
YanivRW <yanivrw2@gmail.com>
2026-05-25 15:49:56 +0300

    fix: use correct Web OAuth client ID for Google Sign-In

--- app/src/main/kotlin/com/yanivrw/lessscreen/GoogleConfig.kt
+++ app/src/main/kotlin/com/yanivrw/lessscreen/GoogleConfig.kt
@@ -2,5 +2,5 @@ package com.yanivrw.lessscreen
 
 object GoogleConfig {
     const val WEB_CLIENT_ID =
-        "846082142354-mp2urpnh7e1t3aqav4os64td2von132n.apps.googleusercontent.com"
+        "846082142354-t0uud57mv19lt618nmto47jent3hut81.apps.googleusercontent.com"
 }
